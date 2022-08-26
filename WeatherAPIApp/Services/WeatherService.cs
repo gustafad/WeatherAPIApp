db58f9e67c332e8dafd43547203a94e3e0e6058f
gustafad <gustafad@student.gu.se>
2022-08-26 16:34:12 +0200

    Add project files.

---
+++ WeatherAPIApp/Services/WeatherService.cs
@@ -0,0 +1,16 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using WeatherAPIApp.Models;
+
+namespace WeatherAPIApp.Services
+{
+    public class WeatherService
+    {
+        public static void CurrentWeather()
+        {
+            CurrentWeatherDTO cw = null;
+        }
+    }
+}
